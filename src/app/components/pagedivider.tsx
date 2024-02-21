0e199782f9f602d7365d7243ade4ee8ac0e8e452
jonas-kgomo <jonaskgmoo@gmail.com>
2024-02-21 12:03:04 +0200

    compare button

--- src/app/components/pagedivider.tsx
+++ src/app/components/pagedivider.tsx
@@ -4,20 +4,35 @@ import {
   ResizablePanel,
   ResizablePanelGroup,
 } from "../../components/ui/divider";
+import { Button } from "@/components/ui/button";
 
 export default function ResizableDemo() {
   return (
     <div className="flex rounded-lg border">
-      <div className="flex w-[100px] bg-slate-100  h-[800px] items-center justify-center p-6">
+      <div className="flex w-96 mx-4 bg-slate-100  h-[800px] items-center justify-center p-6">
         <span className="font-semibold">One</span>
       </div>
-      <div className="flex rounded-xl  bg-[#09131F] h-full w-full grid grid-cols-3 gap-4  items-center justify-center p-6">
-        {["1", "2", "3", "4", "5"].map((e: any) => (
-          <Card className="w-56 h-56 rounded-lg">
-            {" "}
-            <p className="font-medium">Solar Panel {e} </p>
-          </Card>
-        ))}
+      <div className="grid">
+        <div className="flex rounded-2xl  bg-[#09131F] h-full w-full grid grid-cols-3 gap-4  items-center justify-center p-6">
+          {["1", "2", "3", "4", "5"].map((e: any) => (
+            <Card className="w-56 h-56 rounded-lg">
+              {" "}
+              <p className="font-medium">Solar Panel {e} </p>
+            </Card>
+          ))}
+        </div>
+        <Button
+          className="w-56 bg-[#09131F] rounded-xl text-white my-4"
+          size={"sm"}
+          variant={"default"}
+        >
+          Compare
+        </Button>
+        <div>
+          <div className="flex h-full items-center justify-center p-6">
+            <span className="font-semibold">Three</span>
+          </div>
+        </div>
       </div>
     </div>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,14 +3,10 @@ import ResizableDemo from "./components/pagedivider";
 
 export default function Home() {
   return (
-    <main className="  bg-gray-100/20  ">
+    <main className=" max-w-7xl mx-auto p-6  bg-gray-100/20  ">
       {/* <MapBox /> */}
 
       <ResizableDemo />
-
-      <div className="flex h-full items-center justify-center p-6">
-        <span className="font-semibold">Three</span>
-      </div>
     </main>
   );
 }
